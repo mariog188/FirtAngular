f13acc8c1a9128213d346da357df97058e795c9c
Mario Alejandro Gutierrez <mgutierrez@ilimitada.co>
2017-06-30 13:22:31 -0500

    Guard and pipes

--- app/app.module.ts
+++ app/app.module.ts
@@ -5,17 +5,34 @@ import { AppComponent }  from './app.component';
 import { ProductListComponent } from './products/product-list.component';
 import { ProductFilterPipe } from './products/product-filter.pipe';
 import { StarComponent } from './shared/star.component';
+import { HttpModule } from '@angular/http';
+import { WelcomeComponent } from './home/welcome.component';
+import { ProductDetailComponent } from './products/product-detail.component';
+import { RouterModule } from '@angular/router';
+import { ProductDetailGuard } from './products/product-guard.service';
+
 
 @NgModule({
   imports: [ 
     BrowserModule,
-    FormsModule
+    FormsModule,
+    HttpModule,
+    RouterModule.forRoot([
+      { path: 'products', component: ProductListComponent },
+      { path: 'product/:id', canActivate:[ ProductDetailGuard ], component: ProductDetailComponent },
+      { path: 'welcome', component: WelcomeComponent },
+      { path: '', redirectTo: 'welcome', pathMatch: 'full' },
+      { path: '**', redirectTo: 'welcome', pathMatch: 'full' }
+    ])
    ],
   declarations: [ 
     AppComponent,
+    WelcomeComponent,
+    ProductDetailComponent,
     ProductListComponent,
     ProductFilterPipe,
     StarComponent ],
-  bootstrap: [ AppComponent ]
+  providers: [ ProductDetailGuard],
+  bootstrap: [ AppComponent ]  
 })
 export class AppModule { }
